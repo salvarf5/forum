0a12343573eaac5dccc6cb6f5c25921638884c8d
salvarf5 <54718360+salvarf5@users.noreply.github.com>
2019-12-10 15:55:37 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <title>Foro</title>
-  <link rel="stylesheet" type="text/css" href="estilos/style.css"/>
-  <link rel="stylesheet" type="text/css" href="estilos/style.php"/>
+    <meta charset="utf-8">
+    <title>Foro</title>
+    <link rel="stylesheet" type="text/css" href="estilos/style.css" />
+    <link rel="stylesheet" type="text/css" href="estilos/style.php" />
 </head>
-	
-	<body>
-		<div id="wraper">
-			<h1 align="center">Salvador - Foro</h1><br />
-            
-<?php
+
+<body>
+    <div id="wraper">
+        <h1 align="center">Salvador - Foro</h1>
+        <br />
+
+        <?php
 session_start();
 ?>
-<ul style="color: #f00; text-align:center;">
-<?php
+            <ul style="color: #f00; text-align:center;">
+                <?php
 if(isset($_GET['mensaje'])){ 
 	$mensaje = $_GET['mensaje'];
 	echo $mensaje;}
 ?>
-</ul>
-<?php
+            </ul>
+            <?php
 if (isset($_GET['status'])){
         if ($_GET['status'] == 'reg_success'){
         echo "<script>alert('Usuario creado con exito. Recibirá  un email de confirmacion de registro.')</script>";
@@ -29,10 +31,10 @@ if (isset($_GET['status'])){
         echo "<script>alert('Contraseña cambiada con exito.')</script>";
         }
         }
-			
+
 ?>
-<div id="iniciosesion">
-<?php
+                <div id="iniciosesion">
+                    <?php
 if (!isset($_SESSION['uid'])){
     echo "<form action='/foro/usuarios/login_parse.php' method='post'>
     <p>Usuario: <input type='text' name='username' pattern='[A-Za-z0-9_-]{1,15}' required autocomplete='off'/> &nbsp;
@@ -45,13 +47,13 @@ if (!isset($_SESSION['uid'])){
 }else{
         echo "<p>Has iniciado sesion como <i><a data-tooltip='Perfil' href='/foro/usuarios/perfil.php'>".$_SESSION['username']."</a></i> &bull; <input type='submit' value='Cerrar sesion' onClick=\"window.location = 
     '/foro/usuarios/logout_parse.php'\" /></p>";
-        
+
 }
 ?>
-</div>
-<hr />
-<div id="categorias">
-<?php
+                </div>
+                <hr />
+                <div id="categorias">
+                    <?php
 include_once("conexion.php");
 $sql = "SELECT * FROM categorias ORDER BY categoria_titulo ASC";
 $res = mysqli_query($con, $sql) or die(mysqli_error());
@@ -69,7 +71,8 @@ if (mysqli_num_rows($res) > 0){
 }
 ?>
 
-</div>
-</div>
+                </div>
+    </div>
 </body>
+
 </html>
